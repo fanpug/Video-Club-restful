2f83a353833e4eeb8b62a2e1513cdb771ad285f4
Humberto <a320669@uach.mx>
2021-10-26 20:04:47 -0600

    Se agregan las rutas de movies, members, copies y bookings

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/actors');
+const controller = require('../controllers/bookings');
 
 /* GET users listing. */
 router.get('/:page?', controller.list);
